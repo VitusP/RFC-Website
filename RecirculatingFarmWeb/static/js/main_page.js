8928b417e8ffcc466c8d6493c7c094ad500a4543
vaputra <vaputra@ncsu.edu>
2019-12-28 14:23:00 -0500

    Initial project push

---
+++ RecirculatingFarmWeb/static/js/main_page.js
@@ -0,0 +1,13 @@
+/**
+ * JavaScript Document for main_project app
+ * @author Vitus Putra
+ * 12/28/2019
+ */
+
+ /**
+  * Action on page loading
+  * M is the materialize object 
+  */
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+});
