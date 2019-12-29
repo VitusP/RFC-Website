434f4bd4256d9b78b7d9cbd0dd6c1921334264e8
vaputra <vaputra@ncsu.edu>
2019-12-29 17:48:18 -0500

    Create baseline model (Images, Contacts, Message)

--- RecirculatingFarmWeb/main_page/admin.py
+++ RecirculatingFarmWeb/main_page/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import GalleryImage, Contact, Message
 # Register your models here.
+admin.site.register(GalleryImage)
+admin.site.register(Contact)
+admin.site.register(Message)
--- RecirculatingFarmWeb/main_page/models.py
+++ RecirculatingFarmWeb/main_page/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Recirculating Farm Gallery image
+class GalleryImage(models.Model):
+    # Image Title
+    img_title = models.CharField(max_length=50, null=True)
+    # Image description
+    img_description = models.TextField(max_length=200, null=True)
+    # Image
+    image = models.ImageField(upload_to='images/', null=True)
+
+# Contact of user
+class Contact(models.Model):
+    # First Name
+    first_Name = models.CharField(max_length=50, null=True)
+    # Last Name
+    last_Name = models.CharField(max_length=50, null=True)
+    # Email Address
+    email_Address = models.CharField(max_length=50, null=True)
+
+# Message unique for a Contact
+class Message(models.Model):
+    # Message
+    message = models.TextField(max_length=200, null=True)
